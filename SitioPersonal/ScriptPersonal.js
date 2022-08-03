0a94087ffccbb5a6b9b87e40f74db9d4a89b22ba
Mauro Fitermann <90213477+maurofitermann@users.noreply.github.com>
2022-08-03 11:54:06 -0300

    Primer Commit de mi sitio personal

---
+++ SitioPersonal/ScriptPersonal.js
@@ -0,0 +1,27 @@
+
+
+
+
+
+/*
+var counterContainer = document.querySelector(".website-counter");
+
+var visitCount = localStorage.getItem("page_view");
+visitCount = 1;
+
+//Add entry for key="page_view"
+localStorage.setItem("page_view", 1);
+visitCount = Number(visitCount) + 1;
+
+// Update local storage value
+localStorage.setItem("page_view", visitCount);
+counterContainer.innerHTML = visitCount;
+
+// Adding onClick event listener
+resetButton.addEventListener("click", () => {
+    visitCount = 1;
+    localStorage.setItem("page_view", 1);
+    counterContainer.innerHTML = visitCount;
+  });
+*/
+
